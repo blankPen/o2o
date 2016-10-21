8d551270b3841012dd072e247ccd4fbf309f69b5
pengzhen <pengzhen@parim.net>
2016-10-21 18:26:26 +0800

    还原 topnav/index

---
+++ src/components/common/TopNav/index.jsx
@@ -0,0 +1,66 @@
+/*
+ * @Author: pengzhen
+ * @Date:   2016-10-17 20:20:16
+ * @Desc: this_is_desc
+ * @Last Modified by:   pengzhen
+ * @Last Modified time: 2016-10-21 17:51:07
+ */
+
+'use strict';
+import './index.less';
+import React from 'react';
+import {
+    connect
+} from 'react-redux';
+import { Link } from 'react-router';
+import UserBox from 'components/common/UserBox';
+
+function mapStateToProps({common}) {
+    return {
+        position: common.position
+    };
+}
+
+export class TopNav extends React.Component {
+    static propTypes = {
+        name: React.PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { position,userInfo } = this.props;
+        return (
+            <div className='top-nav'>
+                <div className="nav-content">
+                    <div className="nav-left">
+                        <i className="fa fa-map-marker marker-icon"></i>
+                        <span className='city'>{position.city}</span>
+                        <span className='area'>{position.title || position.address}</span>
+                        <Link to='/map' className='change-address'>[切换地址]</Link>
+                    </div>
+                    <div className="nav-right">
+                        <UserBox/>
+                        <div className="moblie-home">
+                            <div className="moblie">
+                                <i className="fa fa-mobile-phone mr5"></i>
+                                手机版
+                            </div>
+                            <div className="home">
+                                <i className="fa fa-home mr5"></i>
+                                美团网
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    // Implement map dispatch to props
+)(TopNav)
