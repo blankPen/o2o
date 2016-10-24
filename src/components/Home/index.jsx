06dbdcddd25b01921d252bbc8d34d8f0f9771463
pengzhen <pengzhen@parim.net>
2016-10-24 11:47:36 +0800

    DIalog

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -14,6 +14,7 @@ import {
     Checkbox,
     Select
 } from 'antd';
+
 const CheckboxGroup = Checkbox.Group;
 const Option = Select.Option;
 
---
+++ src/components/common/Dialog/index.jsx
@@ -0,0 +1,37 @@
+/*
+ * @Author: pengzhen
+ * @Date:   2016-10-24 11:33:55
+ * @Desc: this_is_desc
+ * @Last Modified by:   pengzhen
+ * @Last Modified time: 2016-10-24 11:43:11
+ */
+
+'use strict';
+import './index.less';
+import React from 'react';
+import { Modal } from 'antd';
+
+export default class Dialog extends React.Component {
+    static propTypes = {
+        name: React.PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Modal
+                title='收货地址'
+                className={'dialog'+(this.props.className || '')}
+                visible={this.props.visible}
+                footer={undefined}
+            >
+                <div className="dialog-body">
+                    {this.props.children}
+                </div>
+            </Modal>
+        );
+    }
+}
