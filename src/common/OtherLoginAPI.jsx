699f392ec2eda4f14e93c9fc908564a89da1f17c
pengzhen <pengzhen@parim.net>
2016-11-02 11:26:49 +0800

    三方登录

--- src/common/OtherLoginAPI.jsx
+++ src/common/OtherLoginAPI.jsx
@@ -3,16 +3,17 @@
  * @Date:   2016-11-01 15:34:55
  * @Desc: this_is_desc
  * @Last Modified by:   pengzhen
- * @Last Modified time: 2016-11-01 17:58:17
+ * @Last Modified time: 2016-11-02 11:25:03
  */
 
 'use strict';
 import loadJS from 'common/utils/loader';
 import ajax from 'common/Ajax';
 
+const redirectUri = encodeURIComponent(location.protocol +'//'+location.host+'/#/login');
 const TencentConfig = {
-    sdk: 'http://qzonestyle.gtimg.cn/qzone/openapi/qc_loader.js',
-    appid: 123,
+    appid: 101244672,
+    appkey: 'dfd8be1247c2e27237f012f696be223b',
 };
 
 const WxConfig = {
@@ -21,13 +22,28 @@ const WxConfig = {
     secret: 'd38be49198dbd6df5fb347f6c231c96f',
 };
 
-export function QQLogin() {
-
-}
+export const QQLogin = {
+    init(callback) {
+        // var script = document.createElement("script");
+        // script.src = TencentConfig.sdk;
+        // document.body.appendChild(script);
+    },
+    showPopup(){
+        let QC = window.QC;
+        QC.Login.showPopup({
+            appId: TencentConfig.appid,
+            redirectURI: redirectUri
+        });
+    },
+    login(){
+        var link = `https://graph.qq.com/oauth2.0/authorize?response_type=code&state=1&client_id=${TencentConfig.appid}&redirect_uri=${redirectUri}&display=mobile`;
+        window.open(link);
+    }
+};
 export const WeixinLogin = {
-    init: function(callback) {
+    init(callback) {
         loadJS(WxConfig.sdk, function() {
-            callback();
+            callback && callback();
         });
     },
     createCode(id) {
@@ -35,37 +51,37 @@ export const WeixinLogin = {
             id: id,
             appid: WxConfig.appid,
             scope: "snsapi_login",
-            redirect_uri: encodeURIComponent('http://testo2o.leimingtech.com/leimingtech-seller/login')
+            redirect_uri: redirectUri
         });
     },
-    getUserInfo(code,callback){
-        this.getAccessToken(code,({access_token,openid})=>{
+    getUserInfo(code, callback) {
+        this.getAccessToken(code, ({
+            access_token,
+            openid
+        }) => {
             ajax({
                 url: `/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`,
-                success: function(res){
-                    if(res.errcode){
-                        console.log("getUserInfo",res.errcode +":"+res.errmsg);
-                    }else{
+                success: function(res) {
+                    if (res.errcode) {
+                        console.log("getUserInfo", res.errcode + ":" + res.errmsg);
+                    } else {
                         callback && callback(res);
                     }
                 }
             })
         });
     },
-    getAccessToken(code,callback){
+    getAccessToken(code, callback) {
         ajax({
             type: 'get',
             url: `/sns/oauth2/access_token?appid=${WxConfig.appid}&secret=${WxConfig.secret}&code=${code}&grant_type=authorization_code`,
-            success: function(res){
-                if(res.errcode){
-                    console.log("getAccessToken",res.errcode +":"+res.errmsg);
-                }else{
+            success: function(res) {
+                if (res.errcode) {
+                    console.log("getAccessToken", res.errcode + ":" + res.errmsg);
+                } else {
                     callback && callback(res);
                 }
             }
         })
     }
 };
-
-
-
--- src/components/SignPage/OtherLogin/index.jsx
+++ src/components/SignPage/OtherLogin/index.jsx
@@ -3,7 +3,7 @@
  * @Date:   2016-11-01 16:18:59
  * @Desc: this_is_desc
  * @Last Modified by:   pengzhen
- * @Last Modified time: 2016-11-01 18:14:24
+ * @Last Modified time: 2016-11-01 21:57:10
  */
 
 'use strict';
@@ -11,7 +11,7 @@ import './index.less';
 import React from 'react';
 import History from 'common/History';
 import * as DomUtils from 'common/utils/dom';
-import { WeixinLogin } from 'common/OtherLoginAPI';
+import { QQLogin,WeixinLogin } from 'common/OtherLoginAPI';
 import { thirdLogin } from 'actions/SignPageAction';
 
 export default class OtherLogin extends React.Component {
@@ -28,6 +28,7 @@ export default class OtherLogin extends React.Component {
     componentWillMount() {
         const code = DomUtils.getQueryString('code');
         if(code){
+            History.replace('/login');
             WeixinLogin.getUserInfo(code,(res)=>{
                 console.log(res)
                 this.props.dispatch(thirdLogin({
@@ -39,9 +40,8 @@ export default class OtherLogin extends React.Component {
                 }))
             })
         }
-        WeixinLogin.init((obj)=>{
-            this.wx = obj;
-        });
+        QQLogin.init();
+        WeixinLogin.init();
     }
     toggleWx=()=>{
         this.setState({
@@ -52,18 +52,52 @@ export default class OtherLogin extends React.Component {
         WeixinLogin.createCode('wexin-code');
         this.toggleWx();
     }
+    qqLogin=()=>{
+        QQLogin.login();
+        // QQLogin.showPopup();
+        // QC.Login({
+        //        btnId:"qqLoginBtn"   //插入按钮的节点id
+        // });
+        // QC.Login({
+        //     //btnId：插入按钮的节点id，必选
+        //     btnId: "qqLoginBtn",
+        //     //用户需要确认的scope授权项，可选，默认all
+        //     scope: "all",
+        //     //按钮尺寸，可用值[A_XL| A_L| A_M| A_S|  B_M| B_S| C_S]，可选，默认B_S
+        //     size: "A_XL"
+        // }, function(reqData, opts) { //登录成功
+        //     console.log(reqData)
+        //     // //根据返回数据，更换按钮显示状态方法
+        //     // var dom = document.getElementById(opts['btnId']),
+        //     //     _logoutTemplate = [
+        //     //         //头像
+        //     //         '<span><img src="{figureurl}" class="{size_key}"/></span>',
+        //     //         //昵称
+        //     //         '<span>{nickname}</span>',
+        //     //         //退出
+        //     //         '<span><a href="javascript:QC.Login.signOut();">退出</a></span>'
+        //     //     ].join("");
+        //     // dom && (dom.innerHTML = QC.String.format(_logoutTemplate, {
+        //     //     nickname: QC.String.escHTML(reqData.nickname),
+        //     //     figureurl: reqData.figureurl
+        //     // }));
+        // }, function(opts) { //注销成功
+        //     alert('QQ登录 注销成功');
+        // });
+    }
     render() {
         return (
             <div className="other-login">
                 <div className="title">其他方式登录</div>
                 <div className="btn-box">
-                    <a className="btn-qq"><i className="fa fa-qq"></i></a>
+                    <a className="btn-qq" onClick={this.qqLogin}><i className="fa fa-qq"></i></a>
                     <a className="btn-wechat" onClick={this.wechatLogin}><i className="fa fa-wechat"></i></a>
                 </div>
                 <div className={"wx-wrap "+(this.state.wx?'show':'')}>
                     <i onClick={this.toggleWx} className='ant-modal-close-x'></i>
                     <div id="wexin-code"></div>
                 </div>
+                <div id="qqLoginBtn"></div>
             </div>
         );
     }
